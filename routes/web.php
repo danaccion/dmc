afae3b048be2b4388c94b50adf8d1c5172f3e3ae
dan <dan@webfuelagency.com>
2023-03-30 19:16:02 +0800

    PensoPayController

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('pensopay', App\Http\Controllers\pensopayController::class);
+
+Route::get('/pensopay', [App\Http\Controllers\pensopayController::class, 'pensopay'])->name('pensopay');
