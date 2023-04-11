8aad6abd911c228aa9e0a17bc422a2ba73a45cc3
dan <dan@webfuelagency.com>
2023-04-11 22:55:38 +0800

    success callback gateway error

---
+++ resources/views/pensopay/success.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="alert alert-success" role="alert">
+        Payment action was successful.
+    </div>
+</div>
+@endsection
