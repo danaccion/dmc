d3d1ae561f396ddc7657ca67a374182d83372bf8
Robert Aeron Salcedo <illumizoldyck1696@gmail.com>
2023-04-24 18:13:47 +0800

    Added Admin Seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        User::create([
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('administrator'),
+            'name' => 'Admin',
+        ]);
+    }
+}
