63f1e0413c247ec12d38b999f753a618863105b4
dan <dan@webfuelagency.com>
2023-05-10 17:28:36 +0800

    company name

--- resources/views/clients/table.blade.php
+++ resources/views/clients/table.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row card col-md-12 shadow-sm border-0">
         <div class="card-body">
-                {{$cif_table}}
+                <?php echo $cif_table; ?>
         </div>
     </div>
 </div>
