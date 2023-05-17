2b251fd5717968dfdb4848b5bce29a6105228809
dan <dan@webfuelagency.com>
2023-05-17 19:16:54 +0800

    for status

--- app/Http/Controllers/QuickPayController.php
+++ app/Http/Controllers/QuickPayController.php
@@ -126,6 +126,7 @@ class QuickPayController extends Controller
         $url = '/payments/' . $id . '/link';
         $payments = $client_Quikpay->request->put($url, $form);
         $status = $payments->httpStatus();
+        dd($status);
         if ($status == 200) {
             $jsonObj = json_encode($payments);
             $responseData = json_decode($jsonObj);
