6f0c67764a10ae6e6e337e811c36820c30ca688a
dan <dan@webfuelagency.com>
2023-05-17 17:01:33 +0800

    search

--- resources/views/clients/table.blade.php
+++ resources/views/clients/table.blade.php
@@ -3,9 +3,16 @@
 @section('content')
 <div class="container">
     <div class="row card col-md-12 shadow-sm border-0">
-        <div class="card-body">
-                <?php echo $cif_table; ?>
-        </div>
+    <div class="card-body">
+        <form action="" method="GET">
+            <div style="float: right;"> 
+                <input type="text" name="search" placeholder="Search">
+                <button type="submit">Search</button>
+            </div>
+        </form>
+        <?php echo $cif_table; ?>
+    </div>
+
     </div>
 </div>
 
