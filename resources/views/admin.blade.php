66bc26bd2b83c454e6a82fdd062ecc74e962558c
dan <dan@webfuelagency.com>
2023-05-18 17:44:22 +0800

    isset

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -94,6 +94,7 @@
         </div>
         <div class="col-md-6" style="padding-left:20px; font-size:15px;">
         <h2>Create New Transaction</h2>
+        @if(isset($client))
         <form method="post" action="{{ route('admin.payment.store',$client) }}"  enctype="multipart/form-data">
                 @csrf
 
@@ -187,6 +188,7 @@
                 <br>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
+            @endif
         </div>
     </div>
 </div>
