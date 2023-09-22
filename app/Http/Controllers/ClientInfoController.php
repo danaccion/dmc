3b7b7d35e4a936e6049afc07977dae3d48e308e2
raqz <raquel.galleros.dc@gmail.com>
2023-09-22 22:22:44 +0800

    modify history tables

--- app/Http/Controllers/ClientInfoController.php
+++ app/Http/Controllers/ClientInfoController.php
@@ -36,14 +36,14 @@ class ClientInfoController extends Controller
                             <a href="?search=' . $search . '&sort_by=id&sort_order=asc"><i class="bi bi-arrow-up"></i></a>
                             <a href="?search=' . $search . '&sort_by=id&sort_order=desc"><i class="bi bi-arrow-down"></i></a>
                         </th>
-                        <th>
+                        <!-- <th>
                             Client Id
                             <a href="?search=' . $search . '&sort_by=client_id&sort_order=asc"><i class="bi bi-arrow-up"></i></a>
                             <a href="?search=' . $search . '&sort_by=client_id&sort_order=desc"><i class="bi bi-arrow-down"></i></a>
                         </th>
                         <th>
                             Order Id
-                        </th>
+                        </th> -->
                         <th>
                             Company Name
                         </th>
@@ -102,8 +102,8 @@ class ClientInfoController extends Controller
             $count++;
             $output .= "<tr>";
             $output .= "<td class='text-muted fw-bold'>" . $count . "</td>";
-            $output .= "<td class='text-muted fw-bold'>" . $item->id . "</td>";
-            $output .= "<td class='text-muted fw-bold'>" . optional($item->client->orderId)->id . "</td>";
+           // $output .= "<td class='text-muted fw-bold'>" . $item->id . "</td>";
+           // $output .= "<td class='text-muted fw-bold'>" . optional($item->client->orderId)->id . "</td>";
             $output .= "<td class='text-muted fw-bold'>" . optional($item->client)->name . "</td>";
             $output .= "<td class='text-muted fw-bold'>" . optional($item->client)->pay_no . "</td>";
             $output .= '<td><button id="view" class="view btn btn-primary" value="' . $item->invoice_no . '">' . $button . ' ' . $item->invoice_no . '</button></td>';
